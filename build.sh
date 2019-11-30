95dcbc7d9adaac89ced6e7eda09f6ba3ee8d8c43
Naveed ul Islam <naveed@alislam.org>
2019-11-30 18:41:42 -0500

    Build pipeline updated to exclude tarball generation process.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 meteor update
 npm update
 meteor update --all-packages
-meteor build ../HQVC_ServerConfig/home/centos/upload/ --architecture os.linux.x86_64 --server https://holyquran.site
+meteor build ../HQVC_ServerConfig/app/HQVC_Advance/2.2 --architecture os.linux.x86_64 --server https://holyquran.site --directory
