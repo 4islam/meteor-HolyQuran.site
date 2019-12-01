39768126f6e535e5ce9c9b0d9d951b0b9a9fa426
Naveed ul Islam <naveed@alislam.org>
2019-12-01 16:25:10 -0500

    Build script updated

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-meteor update
-npm update
-meteor update --all-packages
-meteor build ../HQVC_ServerConfig/app/HQVC_Advance/2.2 --architecture os.linux.x86_64 --server https://holyquran.site --directory
+if [ -n "$1" ]; then
+  meteor update
+  npm update
+  meteor update --all-packages
+  meteor build ../HQVC_ServerConfig/app/HQVC_Advance/$1 --architecture os.linux.x86_64 --server https://holyquran.site --directory
+else
+  echo "Useage: ./build.sh Target_Folder_Version_Number  (example: ./build.sh 2.2)"
+fi
