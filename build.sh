8fce5c97d2729995e58aa76daf3b5145f16c0bc3
Naveed ul Islam <naveed@alislam.org>
2019-12-03 17:32:32 -0500

    Build file updated to create tar ball.

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ if [ -n "$1" ]; then
   meteor update --all-packages
   meteor build ../HQVC_ServerConfig/app/HQVC_Advance/$1 --architecture os.linux.x86_64 --server https://holyquran.site --directory
 else
-  echo "Useage: ./build.sh Target_Folder_Version_Number  (example: ./build.sh 2.2)"
+  #echo "Useage: ./build.sh Target_Folder_Version_Number  (example: ./build.sh 2.2)"
+  meteor build ../HQVC_ServerConfig/home/centos/upload --architecture os.linux.x86_64 --server https://holyquran.site 
 fi
