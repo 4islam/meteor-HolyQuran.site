df0fd279117cf1e0caa6a2ed5b639eb3214c8788
Naveed ul Islam <naveed@alislam.org>
2019-12-05 16:56:50 -0500

    Allow for file name arg. Removed directory options

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ if [ -n "$1" ]; then
   meteor update
   npm update
   meteor update --all-packages
-  meteor build ../HQVC_ServerConfig/app/HQVC_Advance/$1 --architecture os.linux.x86_64 --server https://holyquran.site --directory
+  #meteor build ../HQVC_ServerConfig/app/HQVC_Advance/$1 --architecture os.linux.x86_64 --server https://holyquran.site --directory
+  meteor build ../HQVC_ServerConfig/home/centos/upload/$1 --architecture os.linux.x86_64 --server https://holyquran.site
 else
   #echo "Useage: ./build.sh Target_Folder_Version_Number  (example: ./build.sh 2.2)"
-  meteor build ../HQVC_ServerConfig/home/centos/upload --architecture os.linux.x86_64 --server https://holyquran.site 
+  meteor build ../HQVC_ServerConfig/home/centos/upload --architecture os.linux.x86_64 --server https://holyquran.site
 fi
