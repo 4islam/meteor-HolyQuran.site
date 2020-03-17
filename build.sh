ededaf949fb582e53729604b74c51b83eb5c85b6
Naveed ul Islam <naveed@alislam.org>
2020-03-16 21:40:19 -0400

    Build pipeline updated

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ else
   #echo "Useage: ./build.sh Target_Folder_Version_Number  (example: ./build.sh 2.2)"
   meteor build ../HQVC_ServerConfig/home/centos/upload --architecture os.linux.x86_64 --server https://holyquran.site
 fi
-scp -P2222 ../HQVC_ServerConfig/home/centos/upload/HQVC_v1.1.tar.gz centos@158.69.72.248:/home/centos/upload
+scp -P2222 ../HQVC_ServerConfig/home/centos/upload/HQVC.tar.gz centos@158.69.72.248:/home/centos/upload
