6aab9b2e54628eb4f37b3ac2c8c9543dc4b5e794
Naveed ul Islam <naveed@alislam.org>
2022-02-04 15:18:48 -0500

    build code updated to append version number automatically

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ meteor update --all-packages
 npx browserslist@latest --update-db
 meteor npm install
 
+ver=`cat package.json | grep version | grep -oE "\"[^\"]+\"," | sed "s/[\",]//g"`
+
 if [ -n "$1" ]; then
   #meteor build ../HQVC_ServerConfig/app/HQVC_Advance/$1 --architecture os.linux.x86_64 --server https://holyquran.site --directory
   meteor build ../HQVC_ServerConfig/home/centos/upload/$1 --architecture os.linux.x86_64 --server https://holyquran.site
@@ -16,9 +18,14 @@ else
   mkdir ../HQVC_ServerConfig/home/centos/upload/build/temp
   tar -xzf ../HQVC_ServerConfig/home/centos/upload/build/*.tar.gz -C ../HQVC_ServerConfig/home/centos/upload/build/temp
   cp Docker* ../HQVC_ServerConfig/home/centos/upload/build/temp
-  docker build  ../HQVC_ServerConfig/home/centos/upload/build/temp -t hqvc_advance
+  docker build  ../HQVC_ServerConfig/home/centos/upload/build/temp -t "hqvc/$ver"
   rm -fr ../HQVC_ServerConfig/home/centos/upload/build/temp
-  echo "to run locally, use the following command:"
-  echo "docker run -it -p 3000:3000 -e ESHOST=docker.for.mac.localhost:9200 hqvc_advance"
+  echo ""
+  echo "--- To run locally, use the following command: ---"
+  echo "docker run -it -p 3000:3000 -e ESHOST=docker.for.mac.localhost:9200 hqvc/$ver"
+  echo ""
+  echo "--- To push to AWS (once you are authenticated to AWS CLI): ---"
+  echo "docker tag hqvc_advance 190285172867.dkr.ecr.us-east-1.amazonaws.com/hqvc:$ver"
+  echo "docker push 190285172867.dkr.ecr.us-east-1.amazonaws.com/hqvc:$ver"
   #scp -P2222 ../HQVC_ServerConfig/home/centos/upload/HQVC.tar.gz centos@158.69.72.248:/home/centos/upload
 fi
